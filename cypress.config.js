e7d2f45e3c260e02891bb3fa145d1e0d3239a9ab
Patricia Costa <patiii.costa@gmail.com>
2024-04-23 15:21:52 -0300

    configurando dependencias

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://raromdb-3c39614e42d4.herokuapp.com/api/v1',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    //swagger
-     baseUrl: 'https://raromdb-3c39614e42d4.herokuapp.com',
-      
-      //não limpa o estado de tela
-      testIsolation: false
   },
 });
+
